b1bdfc5480540ea1fb64cd6fd976f9d5fece61ac
Maximiliano Gomez <alexander34223@gmail.com>
2023-11-04 22:30:37 -0300

    Termine ejercico 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -6,3 +6,18 @@
 // lado B = 5
 // Output: 58
 
+function calcularPerimetro(a,b){
+    return  2*(a + b)
+};
+let ladoA = parseInt(prompt("Ingrese el primer Lado del cuadrado"));
+
+while(isNaN(ladoA)){
+    ladoA = parseInt(prompt('No ingresaste un numero vuelve a intentarlo:'))
+}
+let ladoB = parseInt(prompt("Ingrese el segundo Lado del cuadrados"));
+
+while(isNaN(ladoB)){
+    ladoB = parseInt(prompt('No ingresaste un numero vuelve a intentarlo:'))
+}
+
+document.write(`El perimetro del cuadrado es: ${calcularPerimetro(ladoA,ladoB)}`);
